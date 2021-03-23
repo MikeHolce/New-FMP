9d977a79eafb38d30aac378a255e20ebddaaa56b
lk052508 <80259484+lk0525@users.noreply.github.com>
2021-03-23 12:05:44 +0000

    Pause Code Added
    The pause code has now been added, which brings up the pause UI and stops the player and enemy from moving.

---
+++ Assets/Code/PauseMenuScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public Canvas PauseMenu;
+    public static bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseMenu.enabled = false;
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            PauseMenu.enabled = !PauseMenu.enabled;
+            if (PauseMenu.enabled == true)
+
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        paused = false;
+    }
+}
