7faa2f903c3d97438993af1ba379a2bea0f5635a
MikeHolce <80104873+MikeHolce@users.noreply.github.com>
2021-04-21 16:01:10 +0100

    main fixes

--- Assets/Code/enemyBehaviour.cs
+++ Assets/Code/enemyBehaviour.cs
@@ -111,9 +111,9 @@ public class enemyBehaviour : MonoBehaviour
         {
             regBool = false;
             chargeBool = true;
-            loopCounter += 0.1f;
-            redCounter += 0.1f;
-            if (loopCounter >= 10)             // counter goes up when it gets to 10 it does 1 damage to the player before u have to wait till recieving more damage
+            loopCounter += 0.1f * Time.deltaTime;
+            redCounter += 0.1f * Time.deltaTime;
+            if (loopCounter >= 3)             // counter goes up when it gets to 10 it does 1 damage to the player before u have to wait till recieving more damage
             {
                 chargeBool = false;
                 attackBool = true;
@@ -122,7 +122,7 @@ public class enemyBehaviour : MonoBehaviour
                 loopCounter = 0;
 
             }
-            if (redCounter >= 12)
+            if (redCounter >= 5)
             {
                 Debug.Log("change red");
                 attackBool = false;
