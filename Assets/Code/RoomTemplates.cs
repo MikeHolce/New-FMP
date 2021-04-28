3e6de3041f2c47ecf01810db1a35b4615a73340d
MikeHolce <mh05252443@priestley.ac.uk>
2021-04-28 02:21:34 +0100

    completed

--- Assets/Code/RoomTemplates.cs
+++ Assets/Code/RoomTemplates.cs
@@ -12,5 +12,30 @@ public class RoomTemplates : MonoBehaviour
 
     public GameObject closedRoom;
 
+    public List<GameObject> rooms;
 
+    public float waitTime = 5;
+    private bool spawnedBoss;
+    public GameObject boss;
+
+    void Update()
+    {
+        if (waitTime <= 0 && spawnedBoss == false)
+        {
+            for (int i = 0; i < rooms.Count; i++)
+            {
+                if(i == rooms.Count - 1)
+                {
+                    Instantiate(boss, rooms[i].transform.position, Quaternion.identity);
+                    spawnedBoss = true;
+                }
+            }
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
 }
+
+
