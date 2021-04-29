ea7ebb9ab513793d66583513386500bd37adebb3
MikeHolce <80104873+MikeHolce@users.noreply.github.com>
2021-04-29 11:08:36 +0100

    camera shakes

---
+++ Assets/Code/cameraShake.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraShake : MonoBehaviour
+{
+    public Transform main;
+    public Transform middle;
+    public Transform left;
+    public Transform right;
+
+    public bool shake;
+    public bool startShake;
+    public int stopRepeat;
+
+    public float step;
+    public float moveSpeed;
+
+    public bool goLeft;
+    public bool goRight;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shake = true;
+        moveSpeed = 10;
+        step = moveSpeed * Time.deltaTime; // calculate distance to move
+
+        goLeft = false;
+        goRight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /**
+        if (goLeft == true)
+        {
+            stopRepeat = 1;
+            if (stopRepeat == 1)
+            {
+                stopRepeat = 0;
+                StartCoroutine(shakeLeft());
+            }
+        }
+
+        if (goRight == true)
+        {
+            stopRepeat = 1;
+            if (stopRepeat == 1)
+            {
+                stopRepeat = 0;
+                StartCoroutine(shakeRight());
+            }
+        }
+        **/
+
+        if (Input.GetKeyDown("space"))
+        {
+            shake = true;
+            goLeft = true;
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            shake = false;
+            goLeft = false;
+            goRight = false;
+            main.position = middle.position;
+        }
+
+        if (shake == true)
+        {
+            if (goLeft == true) // left
+            {
+                main.position = Vector3.MoveTowards(transform.position, left.position, step);
+                if (main.position == left.position)
+                {
+                    goLeft = false;
+                    goRight = true;
+
+                }
+            }
+            if (goRight == true) // right
+            {
+                //currentCheckpoint = Checkpoint2;
+                main.position = Vector3.MoveTowards(transform.position, right.position, step);
+                if (main.position == right.position)
+                {
+                    goRight = false;
+                    goLeft = true;
+                }
+            }
+        }
+        
+
+    }
+    /**
+    IEnumerator shakeLeft()
+    {
+        startShake = false;
+        Debug.Log("left");
+        main.position = Vector3.MoveTowards(transform.position, left.position, step);
+        yield return new WaitForSeconds(1);
+        shake = true;
+        StopCoroutine(shakeLeft());
+    }
+    IEnumerator shakeRight()
+    {
+        startShake = false;
+        Debug.Log("right");
+        main.position = Vector3.MoveTowards(transform.position, right.position, step);
+        yield return new WaitForSeconds(1);
+        shake = false;
+        StopCoroutine(shakeRight());
+    }
+    **/
+}
