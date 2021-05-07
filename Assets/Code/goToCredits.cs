1bf19a77d8ca69f23d7deb24f88d45d024692ae4
lk0525 <80259484+lk0525@users.noreply.github.com>
2021-05-07 10:25:31 +0100

    Credits scene is connected to the game now

--- Assets/Code/goToCredits.cs
+++ Assets/Code/goToCredits.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class goToGame : MonoBehaviour
+public class goToCredits : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+
     }
-    public void LoadSampleScene()
+    public void LoadCreditsScene()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
     }
 }
