dccc61ad9b6ebfe25f0d94de6c8656f4fc49a729
lk0525 <80259484+lk0525@users.noreply.github.com>
2021-05-07 11:43:24 +0100

    Objective Added
    When the game starts the player will see the objective and they will collect the some health before they start.

---
+++ Assets/Code/CollectUICode.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectUICode : MonoBehaviour
+{
+    public Canvas Canvas1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas1.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "TaskTrigger_ON")
+        {
+            Canvas1.enabled = true;
+        }
+        if (other.name == "TaskTrigger_OFF")
+        {
+            Canvas1.enabled = false;
+        }
+    }
+}
